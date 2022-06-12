a1b389d143b7141105ab0c0e06042a5b36874d3b
Valerii-Denisov <klerk919191@gmail.com>
2022-06-12 16:21:15 +0300

    Refactoring code. Edit Readme

--- page_loader/page_downloads.py
+++ page_loader/page_downloads.py
@@ -1,7 +1,10 @@
+"""The module contains the main functions for downloading web pages."""
+
+# import logging
 import os
 import re
 import types
-import urllib.parse as parser
+from urllib import parse as parser
 
 import requests
 from bs4 import BeautifulSoup
@@ -12,38 +15,60 @@ SUFFIX = types.MappingProxyType({
     'images': '.png',
     'css': '.css',
     'js': '.js',
-}
-)
+})
 CONTENT_TYPE = types.MappingProxyType({
-    'images': dict(tag='img', pattern=r'png|jpg', linc='src', write='wb'),
-    'css': dict(tag='link', pattern=r'css', linc='href', write='wb'),
-    'js': dict(tag='script', pattern=r'js', linc='src', write='wb'),
-}
-)
+    'images': dict(
+        tag='img',
+        pattern=r'png|jpg',
+        linc='src',
+        write='wb',
+        name_pattern='(?=-jpg|-png)',
+    ),
+    'css': dict(
+        tag='link',
+        pattern=r'css',
+        linc='href',
+        write='wb',
+        name_pattern='(?=-css)',
+    ),
+    'js': dict(
+        tag='script',
+        pattern=r'js',
+        linc='src',
+        write='wb',
+        name_pattern='(?=-js)',
+    ),
+    'html_page': dict(
+        tag='link',
+        pattern=r'^(?!.*css).|html',
+        linc='href',
+        write='wb',
+        name_pattern='',
+    ),
+})
 
 
 def page_download(url, local_path):
+    """
+    Save web page data to a local directory.
+
+    Parameters:
+        url: string;
+        local_path: string
+
+    Returns:
+        Full path to saved contents.
+    """
     directory_path = get_path(url, local_path, 'directory')
     make_directory(directory_path)
     page_content = requests.get(url).text
-    page_content = save_content(
-        page_content,
-        parser.urlparse(url).netloc,
-        directory_path,
-        'images',
-    )
-    page_content = save_content(
-        page_content,
-        parser.urlparse(url).netloc,
-        directory_path,
-        'css',
-    )
-    page_content = save_content(
-        page_content,
-        parser.urlparse(url).netloc,
-        directory_path,
-        'js',
-    )
+    for key in CONTENT_TYPE.keys():
+        page_content = save_content(
+            page_content,
+            parser.urlparse(url).netloc,
+            directory_path,
+            key,
+        )
     html_file_path = get_path(url, local_path, 'html_page')
     with open(html_file_path, 'w') as write_file:
         write_file.write(page_content)
@@ -51,53 +76,102 @@ def page_download(url, local_path):
 
 
 def get_name(raw_address, object_type, home_netloc=''):
+    """
+    Build file name.
+
+    Parameters:
+        raw_address: string;
+        object_type: string;
+        home_netloc: string.
+
+    Returns:
+          File name.
+    """
     url_data = parser.urlparse(raw_address)
     if url_data.netloc:
         raw_name = '{0}{1}'.format(url_data.netloc, url_data.path)
     else:
         raw_name = '{0}{1}'.format(home_netloc, url_data.path)
     name = re.sub(r'(/|[.])', '-', raw_name)
-    if object_type == 'images':
+    if object_type in CONTENT_TYPE.keys():
         element_name = re.search(
-            r"[a-zA-Z\d-]*(?=-jpg|-png)",
-            name,
-        )
-        return element_name.group() + SUFFIX[object_type]
-    elif object_type == 'css':
-        element_name = re.search(
-            r"[a-zA-Z\d-]*(?=-css)",
-            name,
-        )
-        return element_name.group() + SUFFIX[object_type]
-    elif object_type == 'js':
-        element_name = re.search(
-            r"[a-zA-Z\d-]*(?=-js)",
+            r'[a-zA-Z\d-]*{0}'.format(
+                CONTENT_TYPE[object_type]['name_pattern'],
+            ),
             name,
         )
         return element_name.group() + SUFFIX[object_type]
     return name + SUFFIX[object_type]
 
 
-def find_some(html_file, tag):
+def find_some(html_file, tag, resource_link):
+    """
+    Find content from web page.
+
+    Parameters:
+        html_file: string;
+        tag: string;
+        resource_link: string.
+
+    Returns:
+          List of elements.
+    """
+    result_list = []
     soup = BeautifulSoup(html_file, 'html.parser')
-    return soup.find_all(tag)
+    for element in soup.find_all(tag):
+        if resource_link in element.attrs.keys():
+            result_list.append(element)
+    return result_list
 
 
 def make_directory(path):
+    """
+    Create a directory on the specified path.
+
+    Parameters:
+        path: string.
+    """
     if not os.path.isdir(path):
         os.makedirs(path)
 
 
 def get_path(url, local_path, source_type):
+    """
+    Build the full path of the element.
+
+    Parameters:
+        url: string,
+        local_path: string,
+        source_type: string.
+
+    Returns:
+          Path to element.
+    """
     return os.path.join(local_path, get_name(url, source_type))
 
 
 def save_content(content, home_netloc, directory, resource_type):
+    """
+    Save items from the specified list.
+
+    Parameters:
+        content: string;
+        home_netloc: string;
+        directory: string;
+        resource_type: string.
+
+    Returns:
+          String.
+    """
     result = content
-    for element in find_some(result, CONTENT_TYPE[resource_type]['tag']):
+    for element in find_some(
+        result,
+        CONTENT_TYPE[resource_type]['tag'],
+        CONTENT_TYPE[resource_type]['linc'],
+    ):
         if re.search(
-                CONTENT_TYPE[resource_type]['pattern'],
-                element[CONTENT_TYPE[resource_type].get('linc')],
+            CONTENT_TYPE[resource_type]['pattern'],
+            element[CONTENT_TYPE[resource_type].get('linc')],
         ):
             object_url_data = parser.urlparse(
                 element[CONTENT_TYPE[resource_type]['linc']],
@@ -116,8 +190,8 @@ def save_content(content, home_netloc, directory, resource_type):
                     object_url_data.path,
                 )
                 with open(
-                        element_local_path,
-                        CONTENT_TYPE[resource_type]['write'],
+                    element_local_path,
+                    CONTENT_TYPE[resource_type]['write'],
                 ) as write_file:
                     write_file.write(requests.get(element_url).content)
                 result = re.sub(
