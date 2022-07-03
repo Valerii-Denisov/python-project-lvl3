6743fe3b15c2fcd1875717d648f0cd0b24dea569
Valerii-Denisov <klerk919191@gmail.com>
2022-07-03 09:31:43 +0300

    edit main script

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -25,7 +25,7 @@ def main():
     )
     parser.add_argument('address')
     args = parser.parse_args()
-    log.info('Start downloading page: {0}'.format(args.address))
+    print('Start downloading page: {0}'.format(args.address))
     try:
         path = download(args.address, args.output)
     except requests.exceptions.ConnectionError:
@@ -35,7 +35,7 @@ def main():
     except PermissionError:
         sys.exit(1)
     else:
-        log.info('Downloading complete! Path to file: {0}'.format(path))
+        print('Downloading complete! Path to file: {0}'.format(path))
         sys.exit(0)
 
 
