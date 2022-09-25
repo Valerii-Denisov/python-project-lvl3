389e98adad76060084995732a772f48fce88156b
Valerii-Denisov <klerk919191@gmail.com>
2022-09-25 20:52:00 +0300

    refactoring functions

--- page_loader/page_downloads.py
+++ page_loader/page_downloads.py
@@ -4,10 +4,19 @@ import logging
 from urllib import parse as parser
 
 from bs4 import BeautifulSoup
-from page_loader.file_functions import make_directory, save_content
+from page_loader.file_functions import (
+    make_directory,
+    replace_source_link,
+    write_to_file,
+)
 from page_loader.module_dict import CONTENT_TYPE_TAGS
-from page_loader.naming_functions import get_path
-from page_loader.url_functions import get_raw_data
+from page_loader.naming_functions import get_name, get_path
+from page_loader.url_functions import (
+    get_local_content,
+    get_raw_data,
+    get_source_url,
+)
+from progress.bar import Bar
 
 log_pars = logging.getLogger('app_logger')
 
@@ -27,12 +36,44 @@ def download(saving_url, local_path):
     target_directory_path = get_path(saving_url, local_path, 'directory')
     make_directory(target_directory_path)
     for content_tag in CONTENT_TYPE_TAGS.keys():
-        page_html_tree = save_content(
+        element_list = get_local_content(
             page_html_tree,
-            parser.urlparse(saving_url),
-            target_directory_path,
             content_tag,
+            parser.urlparse(saving_url).netloc,
         )
+        bar = Bar(
+            'Start download {0} content. Download: '.format(content_tag),
+            max=len(element_list),
+        )
+        for element in element_list:
+            log_pars.info('Trying to download the item: {0}'.format(
+                element[CONTENT_TYPE_TAGS[content_tag]['linc']],
+            ))
+            name = get_name(
+                element[CONTENT_TYPE_TAGS[content_tag]['linc']],
+                content_tag,
+                parser.urlparse(saving_url).netloc,
+            )
+            element_local_path = '{0}/{1}'.format(target_directory_path, name)
+            element_url = get_source_url(
+                parser.urlparse(saving_url),
+                element,
+                content_tag,
+            )
+            write_to_file(
+                element_local_path,
+                get_raw_data(element_url).content,
+                content_tag,
+            )
+            replace_source_link(
+                element,
+                target_directory_path,
+                name,
+                content_tag,
+            )
+            bar.next()
+            log_pars.info('The item is downloaded.')
+        bar.finish()
     html_file_path = get_path(saving_url, local_path, 'html_page')
     with open(html_file_path, 'w') as write_file:
         write_file.write(page_html_tree.prettify())
